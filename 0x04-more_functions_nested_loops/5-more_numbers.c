e4e5f474be99fc8624090cae50370672ef075caa
John <johnadepelumi@gmail.com>
2022-09-15 23:26:29 +0000

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int more_numbers()
+{
+	int i;
+	int j;
+
+	for(i = 0; i < 10; i++){
+		j = 0
+		while (j <= 14)
+		{
+			if (j <= 9)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}	
+}
