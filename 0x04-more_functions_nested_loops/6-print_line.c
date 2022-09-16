2db668729c1095167baf4103c1ce643caaed064b
John <johnadepelumi@gmail.com>
2022-09-16 01:58:23 +0000

    print more

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line - check args for an int
+ *
+ *@n - int passed into print_line();
+ *
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar("_");
+	}
+	_putchar("\n");
+}
+
